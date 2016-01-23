db258301574d16f9f03c41f3753ae10cafcd1968
Kyle Thompson <kyle@kylekthompson.com>
2016-01-23 16:19:03 -0500

    Home page more or less finished

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def guest_login
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'static_pages#home'
+  get 'guests', to: 'static_pages#guest_login'
 end
